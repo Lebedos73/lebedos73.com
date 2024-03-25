c1993421e276a52c2370197fc5254bbce23fbac7
lebedos73 <lebedos73@gmail.com>
2024-03-25 11:24:16 +0500

    email send works

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+
 const hamburger = document.querySelector('.hamburger'),
   menu = document.querySelector('.menu'),
   closeElem = document.querySelector('.menu__close');
@@ -16,3 +17,81 @@ const counters = document.querySelectorAll('.skills__lines-top-measure'),
 counters.forEach((item, i) => {
   lines[i].style.width = item.innerHTML;
 });
+
+
+
+
+(function ($) {
+  $(function () {
+
+    $('.modal__close').on('click', function () {
+      $('.overlay, .modal').fadeOut('slow');
+    });
+
+    // form validation setings
+
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          email: {
+            required: true,
+            email: true
+          },
+          textarea: {
+            required: true,
+            minlength: 5
+          },
+          agree: {
+            required: true
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста введите своё имя",
+            minlength: jQuery.validator.format("Введите {0} символа")
+          },
+          email: {
+            required: "Пожалуйста введите свою почту",
+            email: "Неправильно введен адрес почты"
+          },
+          textarea: {
+            required: "Пожалуйста напишите сообщение",
+            minlength: jQuery.validator.format("Хотя бы {0} символов")
+          }
+        }
+      });
+    };
+
+    validateForms('form');
+
+
+
+
+
+    $('form').submit(function (e) {
+      e.preventDefault();
+
+      if (!$(this).valid()) {
+        return;
+      };
+
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function () {
+        $(this).find("input").val("");
+        $('form').trigger('reset');
+
+        $('.overlay, #thanks').fadeIn('slow');
+      });
+      return false;
+    });
+
+
+  });
+})(jQuery);
